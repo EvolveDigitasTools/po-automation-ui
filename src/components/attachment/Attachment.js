77df3fe86e132880e2d686821af7c4e8f753bf51
jatin <adriano23012001@gmail.com>
2024-03-06 15:56:44 +0530

    fixed downloading of attachment applied formatting in attachment.js

--- src/components/attachment/Attachment.js
+++ src/components/attachment/Attachment.js
@@ -1,13 +1,38 @@
-import React from 'react';
-import { TextField, IconButton, InputAdornment, FormControl, InputLabel, OutlinedInput } from '@mui/material';
-import { FileDownload, FileUpload } from '@mui/icons-material';
+import React, { useEffect, useState } from "react";
+import { TextField, IconButton, InputAdornment } from "@mui/material";
+import { FileDownload, FileUpload } from "@mui/icons-material";
+import {
+    openInBrowserExtensions,
+    openInBrowserTypes,
+} from "../../utilities/utils";
 
-export default function Attachment({ label, file, updateFile, downloadFile, required, submit, fileType }) {
+export default function Attachment({
+    label,
+    file,
+    updateFile,
+    downloadFile,
+    required,
+    submit,
+    fileType,
+}) {
+    const [blobUrl, setBlobUrl] = useState(null);
+
+    useEffect(() => {
+        // This could be a Blob URL created and stored in state when the component mounts or in response to certain events
+        // const url = URL.createObjectURL(file);
+        // setBlobUrl(url);
+
+        return () => {
+            if (blobUrl) {
+                URL.revokeObjectURL(blobUrl);
+            }
+        };
+    }, [blobUrl]);
+
     const uploadFile = () => {
-        const input = document.createElement('input');
-        input.type = 'file';
-        if(fileType)
-        input.accept = fileType;
+        const input = document.createElement("input");
+        input.type = "file";
+        if (fileType) input.accept = fileType;
 
         input.onchange = (e) => {
             const selectedFile = e.target.files[0];
@@ -15,45 +40,58 @@ export default function Attachment({ label, file, updateFile, downloadFile, requ
         };
 
         input.click();
-    }
+    };
 
     const viewFile = () => {
-        const url = URL.createObjectURL(file);
-        // const a = document.createElement("a");
-        // a.href = url;
-        // a.download = file.name;
-        // a.click();
-        window.open(url, "_blank");
-        // Clean up by revoking the Blob URL
-        URL.revokeObjectURL(url);
-    }
+        const extension = file.name.split(".").pop().toLowerCase();
+        if (
+            openInBrowserTypes.includes(file.type) ||
+            openInBrowserExtensions.includes(extension)
+        ) {
+            const url = URL.createObjectURL(file);
+            window.open(url, "_blank");
+            setBlobUrl(url);
+        } else {
+            const url = URL.createObjectURL(file);
+            const a = document.createElement("a");
+            a.href = url;
+            a.download = file.name;
+            a.click();
+            URL.revokeObjectURL(url);
+        }
+    };
 
     return (
         <TextField
             required={required}
             InputProps={{
                 readOnly: true,
-                endAdornment:
+                endAdornment: (
                     <InputAdornment position="end">
-                        {updateFile && <IconButton
-                            aria-label="Upload a file"
-                            onClick={uploadFile}
-                        >
-                            <FileUpload />
-                        </IconButton>}
-                        {(file || downloadFile) && <IconButton
-                            aria-label="Open File"
-                            onClick={downloadFile ? downloadFile : viewFile}
-                        >
-                            <FileDownload />
-                        </IconButton>}
+                        {updateFile && (
+                            <IconButton
+                                aria-label="Upload a file"
+                                onClick={uploadFile}
+                            >
+                                <FileUpload />
+                            </IconButton>
+                        )}
+                        {(file || downloadFile) && (
+                            <IconButton
+                                aria-label="Open File"
+                                onClick={downloadFile ? downloadFile : viewFile}
+                            >
+                                <FileDownload />
+                            </IconButton>
+                        )}
                     </InputAdornment>
+                ),
             }}
             label={label}
             value={file?.name ? file.name : ""}
             fullWidth
-            size='small'
-            error={(required && submit) ? (file ? false : true) : false}
+            size="small"
+            error={required && submit ? (file ? false : true) : false}
         />
     );
-};
+}
---
+++ src/utilities/utils.js
@@ -0,0 +1,54 @@
+export const openInBrowserTypes = [
+    // Images
+    "image/jpeg",
+    "image/png",
+    "image/gif",
+    "image/svg+xml",
+    "image/webp",
+    // PDF
+    "application/pdf",
+    // Text
+    "text/plain",
+    "text/html",
+    "text/css",
+    "application/javascript",
+    // XML
+    "application/xml",
+    "text/xml",
+    // Video
+    "video/mp4",
+    "video/webm",
+    "video/ogg",
+    // Audio
+    "audio/mpeg",
+    "audio/ogg",
+    "audio/wav",
+];
+
+export const openInBrowserExtensions = [
+    // Images
+    "jpg",
+    "jpeg",
+    "png",
+    "gif",
+    "svg",
+    "webp",
+    // PDF
+    "pdf",
+    // Text
+    "txt",
+    "html",
+    "htm",
+    "css",
+    "js",
+    // XML
+    "xml",
+    // Video
+    "mp4",
+    "webm",
+    "ogg",
+    // Audio
+    "mp3",
+    "ogg",
+    "wav",
+];
